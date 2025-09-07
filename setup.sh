b22c4fe0c320308693adcfbf176f09234d121e9f
Hannes Möhring <acc@hannesmoehring.de>
2025-09-07 14:56:24 +0200

    gogitgo shortcut

--- setup.sh
+++ setup.sh
@@ -3,7 +3,42 @@
 echo "starting setup"
 python -m pip install --upgrade "pip==21.3"
 python -m pip install -r requirements.txt
+python -m pip install gdown
 sudo apt install uvicorn
+
+echo "attempting to retrieve files..."
+mkdir temp
+cd temp
+rsync --progress /mnt/projects/dlhm/mohringhannes/misc/dlhm_prod.tar.gz .
+tar -xzf dlhm_prod.tar.gz
+rm dlhm_prod.tar.gz
+# install project with models
+# unpack
+
+cd ..
+mkdir -p models/t2m
+mkdir -p models/teach
+cd models/t2m
+# install unpacked_conda
+rsync --progress /mnt/projects/dlhm/mohringhannes/final/unpacked_conda.tar.gz .
+tar -xzf unpacked_conda.tar.gz
+rm unpacked_conda.tar.gz
+# unpack
+cd ..
+cd teach
+# install teach_venv
+rsync --progress /mnt/projects/dlhm/mohringhannes/final/teach_venv.tar.gz .
+tar -xzf teach_venv.tar.gz
+rm teach_venv.tar.gz
+# unpack
+cd ../..
+
+mv temp/dlhm_prod/models/t2m/T2M-GPT models/t2m/
+mv temp/dlhm_prod/models/teach/teach models/teach/
+mv temp/dlhm_prod/models/teach/baseline models/baseline/
+
+
+echo "checking model paths"
 
 paths=(
   "models/teach/teach_venv"
